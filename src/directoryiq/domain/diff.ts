072f3628b3f65cc1184f6737eb805d52af69ccb5
Michael Gonzales <michael@cloudalien.com>
2026-04-02 06:54:07 +0000

    chore: remove remaining DirectoryIQ and EcomViper product code

--- src/directoryiq/domain/diff.ts
+++
@@ -1,6 +0,0 @@
-import { DiffRow } from "@/src/directoryiq/domain/types";
-import { buildDescriptionDiff } from "@/src/lib/directoryiq/descriptionDiff";
-
-export function buildDiffRows(originalText: string, proposedText: string): DiffRow[] {
-  return buildDescriptionDiff(originalText, proposedText);
-}
