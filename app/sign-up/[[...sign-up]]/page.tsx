ea1ed6517ebebecb8bc16b1ffbecc1d937a23b0b
Michael Gonzales <michael@cloudalien.com>
2026-04-02 19:44:40 +0000

    fix: repair blank clerk sign-up page

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#070a12] p-6">
-      <SignUp />
+      <SignUp routing="path" path="/sign-up" signInUrl="/sign-in" />
     </div>
   );
 }
